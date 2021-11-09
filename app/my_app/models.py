5c9996b4295f14ae542006623b87424d22a70a26
Alex <a.giavaras@gmail.com>
2021-11-09 16:20:54 +0000

    Update application

--- app/my_app/models.py
+++ app/my_app/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+from .tasks import create_task
+
+DEFAULT_ERROR_EXPLANATION = "No Error"
+
+class TaskModel(models.Model)
+
+	RESULT_OPTIONS = (("PENDING", "PENDING"),
+                          ("SUCCESS", "SUCCESS"),
+                          ("FAILURE", "FAILURE"),)
+
+    	# the task id of the computation
+    	task_id = models.CharField(max_length=300, primary_key=True)
+    	result = models.CharField(max_length=50, choices=RESULT_OPTIONS)
+    	error_explanation = models.CharField(max_length=500, default=DEFAULT_ERROR_EXPLANATION)
+    	
+	class Meta:
+        	db_table = 'tasks'
+        	
+        @staticmethod
+        def create_and_launch(data):
+        	return TaskModel.launch(data)
+        	
+        @staticmethod
+    	def launch(data):
+    		return create_task.delay(data)
+
+        
--- app/my_app/views.py
+++ app/my_app/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import TaskModel
+
 # Create your views here.
 
 def index(request):
 
     template = loader.get_template('my_app/index.html')
     context = {}
+    
+    if request.methd == 'POST':
+    
+    	# get the time to sleep
+    	value_to_sleep = request.POST.get("value-input")
+    	
+    	# create the task
+    	task_id = TaskModel.create_and_launch(data=value_to_sleep)
+    	
+    	return redirect('tasks/{0}'.format(task_id))
+    
+    
     return HttpResponse(template.render(context, request))
 
 
